33e8e6084b85b2c38e7091b2330edf9d6dbc83bf
Diogo Martins <me.martinsfe@gmail.com>
2022-08-10 05:35:40 -0300

    feat: complete items 16, 17 and 18

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const paraInput = document.querySelector('#carta-texto');
 const paraInsert = document.querySelector('#carta-gerada');
 const newLetterButton = document.querySelector('#criar-carta');
 
+// Arrays de estilos
+const arrStyles = ['newspaper', 'magazine1', 'magazine2'];
+const arrSizes = ['medium', 'big', 'reallybig'];
+const arrRotations = ['rotateleft', 'rotateright'];
+const arrSkews = ['skewleft', 'skewright'];
+
 // Funcao que separa palavras
 const splitInput = () => {
   return paraInput.value.split(' ');
@@ -11,19 +17,54 @@ const checkBlanks = () => {
   return paraInput.value.trim().length === 0;
 }
 
+// Funcao que escolhe aleatoriamente as classes
+const randClass = (arr) => {
+  const posDrawn = Math.floor(Math.random() * arr.length);
+  return arr[posDrawn];
+};
+
 // Funcao que gera a carta
 const genLetter = () => {
-  let wordsToInsert = '';
+  paraInsert.innerText = '';
   const splited = splitInput();
   if (checkBlanks()) {
     return paraInsert.innerText = 'Por favor, digite o conteúdo da carta.'
   }
   for (let word of splited) {
-    wordsToInsert += `<span>${word}</span>`;
+    const createSpan = document.createElement('span');
+
+    createSpan.classList.add(
+      randClass(arrStyles),
+      randClass(arrSizes),
+      randClass(arrRotations),
+      randClass(arrSkews)
+    );
+    createSpan.addEventListener('click', changeClass);
+    createSpan.innerHTML = word;
+    paraInsert.append(createSpan);
   }
-  paraInsert.innerHTML = wordsToInsert;
+  const paraCounter = document.querySelector('#carta-contador')
+  paraCounter.innerText = splited.length;
 };
+
+// Funcao que muda a classe do elemento ao clicar
+const changeClass = (e) => {
+  e.target.className = '';
+  e.target.classList.add(
+    randClass(arrStyles),
+    randClass(arrSizes),
+    randClass(arrRotations),
+    randClass(arrSkews)
+  );
+};
+
+// Acessa spans para adicionar eventos
 
 // Eventos
 newLetterButton.addEventListener('click', genLetter);
 
+/* for (let span of spanList) {
+  span.addEventListener('click', changeClass)
+  console.log(span)
+}
+ */
