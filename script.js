303acb0532319e5012f7bfbc4643241c3f323b53
Diogo Martins <me.martinsfe@gmail.com>
2022-08-10 03:42:36 -0300

    feat: complete items 1, 2, 3 and 4

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-/* Start */
+const paraInput = document.querySelector('#carta-texto');
+const paraInsert = document.querySelector('#carta-gerada');
+const newLetterButton = document.querySelector('#criar-carta');
+
+// Funcao que separa palavras
+const splitInput = () => {
+  return paraInput.value.split(' ');
+}
+
+// Funcao que gera a carta
+const genLetter = () => {
+  let wordsToInsert = '';
+  const splited = splitInput();
+  for (let word of splited) {
+    wordsToInsert += `<span>${word}</span>`;
+  }
+  paraInsert.innerHTML = wordsToInsert;
+};
+
+// Eventos
+newLetterButton.addEventListener('click', genLetter);
+
